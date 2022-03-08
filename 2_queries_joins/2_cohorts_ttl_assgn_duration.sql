ba3d5876be3dc4691c4775854ac1597447fffab3
scee10 <schhor05@gmail.com>
2022-03-08 02:48:40 +0000

    added 2 more tables and 2 files of dummy data

--- 2_queries_joins/2_cohorts_ttl_assgn_duration.sql
+++ 2_queries_joins/2_cohorts_ttl_assgn_duration.sql
@@ -4,4 +4,5 @@ JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'FEB12';
 
--- WHERE students.start_date = '2010-02-12';
+
+
---
+++ 3_queries_group-by/6_low_average_completion_time.sql
@@ -0,0 +1,9 @@
+SELECT students.name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration, AVG(assignments.duration) AS average_estimated_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
+GROUP BY students.name
+HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
+ORDER BY average_assignment_duration ASC;
+
