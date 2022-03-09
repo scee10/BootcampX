5756e8519656cc4b8ca73dbc06cb92e5e5a488c1
scee10 <schhor05@gmail.com>
2022-03-09 19:50:25 +0000

    used parameterized queries for students.js and teachers.js

---
+++ bootcamp_app/students.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const queryString = `
+SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+FROM cohorts
+JOIN students ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}%`]
+
+pool.query(queryString, values)
+.then(res => {
+ res.rows.forEach(teacher => {console.log(`${process.argv[2]}: ${teacher.teacher}`)})
+})
